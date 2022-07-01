d88aa0e3cc8d304e958b4f314c36bb9668e6354b
Owain Kenway <o.kenway@ucl.ac.uk>
2022-07-01 17:41:23 +0100

    Various tidy

--- qlm_mult/circuit_mult_c.py
+++ qlm_mult/circuit_mult_c.py
@@ -18,13 +18,13 @@ x = prog.qalloc(reg_size, QInt)
 y = prog.qalloc(reg_size, QInt)
 
 # y = 3, x = 2*y
+y.set_value(3)
 prog.apply(classarith.mult_const(reg_size,reg_size,constant), [y,x])
 
 # Use the most basic, on computer linear algebra engine.
 qpu = PyLinalg()
 
-# Create two versions of the circuit, one doing QFT arith and one doing
-# classical arithmetic.
+# Create the circuit.
 circ_c = prog.to_circ(link=[classarith])
 
 # This is probably boring as we can't inline operators?
