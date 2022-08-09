3d3ebb03579775e5df9ba388e0f8e80898b0e611
Owain Kenway <o.kenway@ucl.ac.uk>
2022-08-09 13:45:08 +0100

    Commit code related to segfault
    See: https://github.com/Qiskit/qiskit-aer/issues/1571

---
+++ qiskit_extended_stabilizer/test_sv.py
@@ -0,0 +1,31 @@
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
+from qiskit.compiler import assemble
+from qiskit.providers.aer import AerError, QasmSimulator
+from qiskit.tools.visualization import plot_histogram
+import sys
+import random   
+
+upper_limit=39
+if len(sys.argv) > 1:
+   upper_limit = int(sys.argv[1])
+
+print(upper_limit)
+circ = QuantumCircuit(upper_limit, upper_limit)
+
+# Initialise with a Hadamard layer
+circ.h(range(upper_limit))
+# Apply some random CNOT and T gates
+qubit_indices = [i for i in range(upper_limit)]
+for i in range(10):
+   control, target, t = random.sample(qubit_indices, 3)
+   circ.cx(control, target)
+   circ.t(t)
+circ.measure(range(upper_limit), range(upper_limit))
+
+qobj = assemble(circ, backend=QasmSimulator(), shots=1)
+
+es_job = QasmSimulator().run(qobj, backend_options={
+        'method': 'statevector'
+        })
+result = es_job.result()
+print('This succeeded?: {}'.format(result.success))
